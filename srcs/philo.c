fca480e7faaa8588aee40aa59aebd788091ef4bd
Phil-Menard <phil.menard1668@gmail.com>
2025-02-12 13:34:59 +0100

    push for school / did functions to understand threads

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,14 +6,50 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:01:01 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/12 12:09:49 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/12 13:34:16 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void *thread_function(void *arg)
+{
+	int value = *(int *)arg;
+	printf("Bonjour depuis le thread ! Valeur reçue : %d\n", value);
+	pthread_exit(NULL); // Termine le thread
+}
+
+pthread_t	*create_philo(int nb_philo)
+{
+	pthread_t	*philos;
+	int			i;
+	int			*arg;
+
+	philos = malloc(nb_philo * sizeof(pthread_t));
+	arg = malloc(nb_philo * sizeof(int));
+	i = 0;
+	while (i < nb_philo)
+	{
+		arg[i] = i * 10;
+		if (pthread_create(&philos[i], NULL, thread_function, (void *)&arg[i]) != 0)
+			printf("Erreur lors de la création du thread\n");
+		i++;
+	}
+	return (philos);
+}
+
 int	main(void)
 {
-	printf("ok\n");
+	pthread_t	*thread;
+	int			nb_philo;
+
+	nb_philo = 3;
+	thread = create_philo(nb_philo);
+	pthread_join(thread[0], NULL);
+	printf("Le thread1 s'est termine\n");
+	pthread_join(thread[1], NULL);
+	printf("Le thread2 s'est termine\n");
+	pthread_join(thread[2], NULL);
+	printf("Le thread3 s'est termine\n");
 	return (0);
 }
--- srcs/philo.h
+++ srcs/philo.h
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:01:15 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/12 12:02:13 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/12 12:41:36 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 # define PHILO_H
 
 # include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
 
 #endif
