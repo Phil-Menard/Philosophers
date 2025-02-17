8f3c01976c0085b06a4b092c2ef0df1df9e95681
Philippe Menard <phil.menard1668@gmail.com>
2025-02-17 12:03:20 +0100

    init philosophers and forks works fine, valgrind OK

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:58:36 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/13 18:14:40 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/17 12:01:55 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,22 @@ void	init_philosophers(t_table *table, int nb_philo)
 	int	i;
 
 	table->philosophers = malloc(nb_philo * sizeof(t_philo));
+	table->forks = malloc(nb_philo * sizeof(pthread_mutex_t));
+	i = 0;
 	while (i < nb_philo)
 	{
-		table->philosophers->id = i + 1;
-		// table->philosophers->left = 
+		pthread_mutex_init(&table->forks[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < nb_philo)
+	{
+		table->philosophers[i].id = i + 1;
+		table->philosophers[i].fork_left = &table->forks[i];
+		if (i == nb_philo - 1)
+			table->philosophers[i].fork_right = &table->forks[0];
+		else
+			table->philosophers[i].fork_right = &table->forks[i + 1];
 		i++;
 	}
 }
@@ -28,10 +40,20 @@ void	init_philosophers(t_table *table, int nb_philo)
 int	main(int argc, char **argv)
 {
 	t_table	table;
-	int	nb_philo;
+	int		nb_philo;
+	int		i;
 
 	(void) argc;
 	nb_philo = ft_atoi(argv[1]);
-	init_philosophers(&table);
+	init_philosophers(&table, nb_philo);
+	printf("%d\n", table.philosophers[0].id);
+	i = 0;
+	while (i < nb_philo)
+	{
+		pthread_mutex_destroy(&table.forks[i]);
+		i++;
+	}
+	free(table.philosophers);
+	free(table.forks);
 	return (0);
 }
--- srcs/philo.h
+++ srcs/philo.h
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:01:15 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/13 18:09:10 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/17 11:45:38 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+typedef struct s_philo
+{
+	pthread_t		thread;
+	pthread_mutex_t	*fork_left;
+	pthread_mutex_t	*fork_right;
+	int				id;
+}	t_philo;
+
 typedef struct s_table
 {
 	pthread_mutex_t	*forks;
 	t_philo			*philosophers;
 }	t_table;
-
-typedef struct s_philo
-{
-	pthread_t		thread;
-	pthread_mutex_t	*left;
-	pthread_mutex_t	*right;
-	int				id;
-}	t_philo;
 
 int	ft_atoi(const char *nptr);
 
