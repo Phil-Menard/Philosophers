0a067ca8093e795c5587306314eabafeb5295f4c
Philippe Menard <phil.menard1668@gmail.com>
2025-02-17 14:35:49 +0100

    handle forks picking

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,30 +6,30 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:58:36 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/17 12:01:55 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/17 14:35:32 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_philosophers(t_table *table, int nb_philo)
+void	init_philosophers(t_table *table)
 {
 	int	i;
 
-	table->philosophers = malloc(nb_philo * sizeof(t_philo));
-	table->forks = malloc(nb_philo * sizeof(pthread_mutex_t));
+	table->philosophers = malloc(table->nb_philo * sizeof(t_philo));
+	table->forks = malloc(table->nb_philo * sizeof(pthread_mutex_t));
 	i = 0;
-	while (i < nb_philo)
+	while (i < table->nb_philo)
 	{
 		pthread_mutex_init(&table->forks[i], NULL);
 		i++;
 	}
 	i = 0;
-	while (i < nb_philo)
+	while (i < table->nb_philo)
 	{
 		table->philosophers[i].id = i + 1;
 		table->philosophers[i].fork_left = &table->forks[i];
-		if (i == nb_philo - 1)
+		if (i == table->nb_philo - 1)
 			table->philosophers[i].fork_right = &table->forks[0];
 		else
 			table->philosophers[i].fork_right = &table->forks[i + 1];
@@ -37,23 +37,60 @@ void	init_philosophers(t_table *table, int nb_philo)
 	}
 }
 
+void	*handle_threads(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) arg;
+	pthread_mutex_lock(philo->fork_left);
+	printf("%d has taken his left fork\n", philo->id);
+	pthread_mutex_lock(philo->fork_right);
+	printf("%d has taken his right fork\n", philo->id);
+	printf("%d is eating\n", philo->id);
+	usleep(500);
+	printf("%d finished eating\n", philo->id);
+	pthread_mutex_unlock(philo->fork_left);
+	pthread_mutex_unlock(philo->fork_right);
+	return (NULL);
+}
+
+int	start_philosophers(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	while (++i < table->nb_philo)
+	{
+		if (pthread_create(&table->philosophers[i].thread, NULL, handle_threads,
+				&table->philosophers[i]))
+		{
+			printf("Erreur lors de la creation du thread.\n");
+			free_philo_and_forks(table, i);
+			return (1);
+		}
+	}
+	i = -1;
+	while (++i < table->nb_philo)
+	{
+		if (pthread_join(table->philosophers[i].thread, NULL))
+		{
+			printf("Erreur lors de l'attente du thread.\n");
+			free_philo_and_forks(table, i);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_table	table;
-	int		nb_philo;
-	int		i;
 
 	(void) argc;
-	nb_philo = ft_atoi(argv[1]);
-	init_philosophers(&table, nb_philo);
-	printf("%d\n", table.philosophers[0].id);
-	i = 0;
-	while (i < nb_philo)
-	{
-		pthread_mutex_destroy(&table.forks[i]);
-		i++;
-	}
-	free(table.philosophers);
-	free(table.forks);
+	table.nb_philo = ft_atoi(argv[1]);
+	init_philosophers(&table);
+	if (start_philosophers(&table) == 1)
+		return (1);
+	free_all(&table);
 	return (0);
 }
--- srcs/philo.h
+++ srcs/philo.h
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:01:15 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/17 11:45:38 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/17 14:33:46 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,11 @@ typedef struct s_table
 {
 	pthread_mutex_t	*forks;
 	t_philo			*philosophers;
+	int				nb_philo;
 }	t_table;
 
-int	ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
+void	free_philo_and_forks(t_table *table, int i);
+void	free_all(t_table *table);
 
 #endif
